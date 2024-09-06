3550ef9693a03b9b4d3c9be2888347378ebe3c14
iamishaqkhan <ishaqpakkaya@gmail.com>
2024-09-06 22:44:06 +0500

    file add

---
+++ datatype-conversion.js
@@ -0,0 +1,33 @@
+
+
+let score = "55"
+ 
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);   
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+//"55" => 55
+//"55asd" => NaN
+// True => 1, False => 0
+
+let isLoggedIn = ""
+
+let BooleanisLoggedIn = Boolean(isLoggedIn)
+
+console.log(typeof BooleanisLoggedIn);
+
+//1 = true, 0 = false
+//"" = false
+// "ishaq" = true
+
+let somenumber = 55
+
+let stringnumber = String(somenumber)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+// 55 = string
+//
---
+++ operations.js
@@ -0,0 +1,41 @@
+
+let Number = 5
+
+let negvalue = -Number
+//console.log(negvalue);
+//-5
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1 + str2
+//console.log(str3);
+// hello world
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2%3)
+// console.log(2**4);
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(2+2 +"1");
+
+// 12
+//12
+// 122
+// 41
+
+let counter = 50
+ 
+counter++
+
+console.log(counter);
+
+let game = 100
+++game
+console.log(game);
+
+
